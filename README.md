4c15cfdd72b2fa0ffbc2c89a7503ab075a9b2921
jorden789 <jordenallcock789@gmail.com>
2018-02-04 17:37:14 +0000

    Addition of basic Vector Based analysis

--- README.md
+++ README.md
@@ -22,3 +22,16 @@ Flow of Operation:
 	|					| CV's are selected ready for further analysis
 	| 6. 
 
+
+CV Analysis Routines:
+
+There exists multiple files, each designed to perform CV analysis using its own distinct methodology. An overview of each file is as below:
+
+	| 1. cv_match_keyword.py		| Analyses the resource request and selects initial keywords
+	| 					| Keywords are also extracted from pre-formatted CV's at runtime
+	|					| Matches are returned based on exact matches between resource request and cv
+	| 2. cv_match_word_vectors.py		| Analyses the resource request and selects initial keywords
+	| 					| Analyses the pre-formatted CV's and extracts keywords
+	| 					| Runs a spacy similarity on the identified keywords in the resource request and the CV to determine 
+	|					| extent of match
+	| 3. cv_match_  
---
+++ cv_match_word_vectors.py
@@ -0,0 +1,102 @@
+#! /usr/bin/env python
+
+####################################################################################################
+# Name: CV Match Routin - Word Vectors on Keywords
+# Author: Jorden Allcock
+#
+# Description: File is responsible for:
+#
+#       - Reading appropriate contents of specified resource request
+#       - Selecting appropriate information from CV (keywords)
+#       - Identifying similarity/match between CV elements and Resource Request elements
+#
+# Parameters:
+#
+#       - p_resource_request_loc        Directory/file location of specified resource request to be
+#                                       analysed
+#
+####################################################################################################
+
+###################################################################################
+# Required imports
+###################################################################################
+
+from __future__ import unicode_literals
+import spacy
+import random
+nlp = spacy.load("en")
+import xlrd
+import os
+
+######################################################################
+# Read Resource Request Contents
+######################################################################
+
+resource_request = xlrd.open_workbook('/home/jallcock/environments/Final-Project/resource_requests/resource_request_1.xlsx')
+request_information = resource_request.sheet_by_index(0)
+
+print('######################################################################')
+print(' Resource Information ')
+print('######################################################################')
+
+print(' ')
+
+job_title = request_information.cell(39, 3).value
+tech_skills_required = request_information.cell(43, 3).value
+bus_skills_required = request_information.cell(46, 3).value
+additional_info = request_information.cell(49, 3).value
+
+print('Required Role: ' + job_title)
+print('Tech Required Skills: ' + tech_skills_required)
+print('Bus. Required Skills: ' + bus_skills_required)
+print('Additional Information: ' + additional_info)
+
+###################################################################################
+# Resource Information Analysis
+###################################################################################
+
+#tech_skills_doc = nlp(tech_skills_required)
+#bus_skills_doc = nlp(bus_skills_required)
+#additional_info_doc = nlp(additional_info)
+all_items_doc = nlp(tech_skills_required + bus_skills_required + additional_info)
+
+for skills_token in all_items_doc:
+    if (skills_token.pos_ == 'NOUN' or skills_token.pos_ == 'PROPN'):
+        print(skills_token, skills_token.pos_)
+
+###################################################################################
+# CV Information Input
+###################################################################################
+
+
+
+
+###################################################################################
+# CV Information Parse
+###################################################################################
+
+
+
+
+
+###################################################################################
+# Word2Vec Compute
+###################################################################################
+
+
+#tokens = nlp(u'sql plsql java oracle ebusiness')
+resource_request_tokens = nlp(all_items_doc.text)
+cv_tokens = nlp(all_items_doc.text)
+
+
+
+for token1 in resource_request_tokens:
+    for token2 in cv_tokens:
+        if ((token1.pos_ == 'NOUN' or token1.pos_ == 'PROPN') and token1.similarity(token2) > 0.4 and token1.similarity(token2) < 0.98):
+            print(token1.text + '|' + token2.text + ' -> ' + str(token1.similarity(token2)))
+
+
+###################################################################################
+# Word2Vec Analysis
+###################################################################################
+
