0d7fb08c8d0b78aedc07622260a8e500747ca0b3
jorden789 <jordenallcock789@gmail.com>
2018-02-20 13:59:43 +0000

    Addition of template files for resource ranking, scoring and resource manager output

---
+++ cv_resource_rank.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+####################################################################################################
+# Name:   CV Link Pull
+# Author: Jorden Allcock
+#
+# Description: File is responsible for:
+#
+#       - Extracting link of CV text from the returned results (from search string parameter)
+#       - Opening link for CV text, extracting and storing text
+#
+# Parameters:
+#
+#       - p_cv_search_string        LiveCareers Search URL
+#
+####################################################################################################
+
+###################################################################################
+# Required imports
+###################################################################################
+
+
